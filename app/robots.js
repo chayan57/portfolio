3586ac23949147ced1f1fc51cdefbdfdf152a47e
chayan57 <ckb8583@gmail.com>
2026-08-21 13:39:00 +0600

    add sitemap and robots

---
+++ app/robots.js
@@ -0,0 +1,11 @@
+export default function robots() {
+  const baseUrl = "https://chayanbiswas.vercel.app";
+
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  const baseUrl = "https://chayanbiswas.vercel.app";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
